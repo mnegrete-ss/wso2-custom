4de15ae61cdbfc9e6d97b7554cca634d2caa58b7
mnegrete <mnegrete@seaburysolutions.com>
2019-10-05 09:33:43 -0300

    broker

---
+++ images/broker/docker-entrypoint-postgresql.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+set -e
+
+# check if the WSO2 non-root user home exists
+test ! -d ${WORKING_DIRECTORY} && echo "WSO2 Docker non-root user home does not exist" && exit 1
+
+# check if the WSO2 product home exists
+test ! -d ${WSO2_SERVER_HOME} && echo "WSO2 Docker product home does not exist" && exit 1
+
+# volume mounts
+config_volume=${WORKING_DIRECTORY}/wso2-config-volume
+
+# replace environnment variables on configuration files
+sed -i "s#{POSTGRESQL_DB_HOST}#${POSTGRESQL_DB_HOST}#g" ${config_volume}/wso2/broker/conf/datasources/master-datasources.xml
+sed -i "s#{POSTGRESQL_DB_PORT}#${POSTGRESQL_DB_PORT}#g" ${config_volume}/wso2/broker/conf/datasources/master-datasources.xml
+
+# call original entrypoint
+( "/home/wso2carbon/docker-entrypoint.sh" )
